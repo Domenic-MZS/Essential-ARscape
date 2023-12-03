7a8b7d2051e9fd2ff01a7278bb181e905ce8b23b
Domenic-MZS <domenicoenriquemeza20014@gmail.com>
2023-12-03 20:56:20 -0300

    fix: support multiple inline space-separeted paths conversion

--- scripts/convert-links.sh
+++ scripts/convert-links.sh
@@ -13,22 +13,24 @@ read -p "Enter your choice (1 or 2): " choice
 
 case $choice in
     1)
+        # fix markdown []() links by replacing - with %20
         echo "Converting Markdown links to Obsidian links..."
-        # fix markdown []() links
-        find "$root_project" -type f -name "*.md" -exec sed -i -E ':a; s/([^!]\[.+\])\((.+)-(.+)\)/\1(\2%20\3)/g; ta' {} +
+        find "$root_project" -type f -name "*.md" -exec sed -i -E ':a;s/(\[[^\)]+\]\([^\)]+#[^\)]+)-([^\)]+)/\1%20\2/;ta' {} +
 
+        # fix img src="" links by adding file path to project file://...
         echo "Converting Markdown local src to Obsidian absolute src..."
-        # fix img src="" links
-        escaped_root_project=$(echo "$root_project" | sed 's/\//\\\//g')
-        find "$root_project" -type f -name "*.md" -exec sed -i -E "s#(src=[\"'])/#\1file://$escaped_root_project#g" {} +
+        root_path=$(sed 's/[&/\]/\\&/g' <<<"$root_project")
+        find "$root_project" -type f -name "*.md" -exec sed -i -E "s#(src=[\"'])/#\1file://$root_path#g" {} +
         ;;
     2)
+        # fix markdown []() links by replacing %20 with -
         echo "Converting Obsidian links to Markdown links..."
-        find "$root_project" -type f -name "*.md" -exec  sed -i -E ':a; s/([^!]\[.+\])\((.+)%20(.+)\)/\1(\2-\3)/g; ta' {} +
+        find "$root_project" -type f -name "*.md" -exec sed -i -E ':a;s/(\[[^\)]+\]\([^\)]+#[^\)]+)%20([^\)]+)/\1-\2/;ta' {} +
 
+        # fix img src="" links by removing file path to project file://...
         echo "Converting Obsidian absolute src to Markdown local src..."
-        escaped_root_folder=$(echo "$root_project" | sed 's/\//\\\//g')
-        find "$root_project" -type f -name "*.md" -exec sed -i -E "s#(src=[\"'])file:.+$escaped_root_folder#\1/#g" {} +
+        root_path=$(sed 's/[^^]/[&]/g; s/\^/\\^/g' <<<"$root_project")
+        find "$root_project" -type f -name "*.md" -exec sed -i -E "s#(src=[\"'])file://$root_path#\1/#g" {} +
         ;;
     *)
         echo "Invalid choice. Please enter 1 or 2."
